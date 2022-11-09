68554f18fb6cf935fcf47fe9052d90fc791467cf
Рашид Аюпов <valentaine1234@gmail.com>
2022-11-09 17:51:07 +0300

    Добавил проверку url'ов profile_unfollow и profile_follow

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -6,7 +6,7 @@ from django.test import Client, TestCase
 from django.urls import reverse
 from django.core.cache import cache
 
-from ..models import Group, Post, Follow
+from ..models import Group, Post
 
 User = get_user_model()
 
